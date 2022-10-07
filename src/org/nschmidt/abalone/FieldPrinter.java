fe8d58d0045d41ba44c1c21e1094b24aa5208f02
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2022-10-07 09:13:43 +0200

    Refactored utiliy class to print a field.

--- src/org/nschmidt/abalone/FieldPrinter.java
+++ src/org/nschmidt/abalone/FieldPrinter.java
@@ -1,5 +1,7 @@
 package org.nschmidt.abalone;
 
+import static java.lang.System.out;
+
 public enum FieldPrinter {
     INSTANCE;
     
@@ -10,36 +12,36 @@ public enum FieldPrinter {
             long fieldValue = Long.remainderUnsigned(nextValue, 3L);
             nextValue = Long.divideUnsigned(nextValue, 3L);
             breakLine(i);
-            System.out.print(fieldValue);
+            out.print(fieldValue);
             printSpaces(1);
         }
         
-        System.out.println();
-        System.out.println();
+        out.println();
+        out.println();
     }
 
     private static void printSpaces(int count) {
         for (int i = 0; i < count; i++) {
-            System.out.print(' ');
+            out.print(' ');
         }
     }
 
     private static void breakLine(int i) {
         switch (i) {
         case 4, 28:
-            System.out.println();
+            out.println();
             printSpaces(2);
             break;
         case 9, 22:
-            System.out.println();
+            out.println();
             printSpaces(1);
             break;
         case 15:
-            System.out.println();
+            out.println();
             printSpaces(0);
             break;
         case 33:
-            System.out.println();
+            out.println();
             printSpaces(3);
             break;
         default:
