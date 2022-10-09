66b4cbb7b6651760b4e753c0d4815974b62cdf96
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2022-10-09 20:33:38 +0200

    Added a detector for triple attack moves.

--- src/org/nschmidt/abalone/MoveDetector.java
+++ src/org/nschmidt/abalone/MoveDetector.java
@@ -1,6 +1,7 @@
 package org.nschmidt.abalone;
 
 import static org.nschmidt.abalone.DoubleAttacker.performDoubleAttack;
+import static org.nschmidt.abalone.TripleAttacker.performTripleAttack;
 import static org.nschmidt.abalone.TripleMover.moveThreeMarbles;
 import static org.nschmidt.abalone.DoubleMover.moveTwoMarbles;
 import static org.nschmidt.abalone.SingleMover.moveSingleMarble;
@@ -12,20 +13,24 @@ public enum MoveDetector {
         final long[] singleMoves = moveSingleMarble(state, player);
         final long[] doubleMoves = moveTwoMarbles(state, player);
         final long[] tripleMoves = moveThreeMarbles(state, player);
-        final long[] attacks = performDoubleAttack(state, player);
+        final long[] doubleAttacks = performDoubleAttack(state, player);
+        final long[] tripleAttacks = performTripleAttack(state, player);
         
         final int singleMoveCount = singleMoves.length;
         final int doubleMoveCount = doubleMoves.length;
         final int tripleMoveCount = tripleMoves.length;
-        final int attackCount = attacks.length;
+        final int doubleAttackCount = doubleAttacks.length;
+        final int tripleAttackCount = tripleAttacks.length;
         final int singleAndDoubleMoveCount = singleMoveCount + doubleMoveCount;
-        final int moveCount = singleAndDoubleMoveCount + tripleMoveCount;
+        final int totalMoveCount = singleAndDoubleMoveCount + tripleMoveCount;
+        final int totalMoveAndDoubleAttackCount = totalMoveCount + doubleAttackCount;
         
-        final long[] result = new long[moveCount + attackCount];
+        final long[] result = new long[totalMoveAndDoubleAttackCount + tripleAttackCount];
         System.arraycopy(singleMoves, 0, result, 0, singleMoveCount);
         System.arraycopy(doubleMoves, 0, result, singleMoveCount, doubleMoveCount);
         System.arraycopy(tripleMoves, 0, result, singleAndDoubleMoveCount, tripleMoveCount);
-        System.arraycopy(attacks, 0, result, moveCount, attackCount);
+        System.arraycopy(doubleAttacks, 0, result, totalMoveCount, doubleAttackCount);
+        System.arraycopy(tripleAttacks, 0, result, totalMoveAndDoubleAttackCount, tripleAttackCount);
         
         return result;
     }
---
+++ src/org/nschmidt/abalone/TripleAttacker.java
@@ -0,0 +1,70 @@
+package org.nschmidt.abalone;
+
+import static org.nschmidt.abalone.Adjacency.adjacency;
+import static org.nschmidt.abalone.Field.lookAtField;
+import static org.nschmidt.abalone.Field.move;
+
+public enum TripleAttacker {
+    INSTANCE;
+    
+    public static long[] performTripleAttack(long state, long player) {
+        final long opponent = player % 2 + 1;
+        final long[] tempResult = new long[54];
+        int attackCount = 0;
+        
+        for (int from = 0; from < 37; from++) {
+            if (player == lookAtField(state, from)) {
+                final int[] neighbourIndicies = adjacency(from);
+                for (int d = 0; d < 6; d++) {
+                    attackCount += tryTripleAttack(state, player, opponent, from, d, attackCount, neighbourIndicies, tempResult);
+                }
+            }
+        }
+        
+        final long[] result = new long[attackCount];
+        System.arraycopy(tempResult, 0, result, 0, attackCount);
+        return result;
+    }
+    
+    private static int tryTripleAttack(long state, long player, long opponent, int from, int dir, int moveCount, int[] neighbourIndicies, long[] tempResult) {
+        final int secondMarbleIndex = neighbourIndicies[dir];
+        // Es ist keine Murmel an dieser Stelle
+        if (secondMarbleIndex == -1) return 0;
+        final long secondMarble = lookAtField(state, secondMarbleIndex);
+        // Die zweite Murmel ist nicht vom selben Spieler
+        if (secondMarble != player) return 0;
+        
+        final int[] secondMarbleNeighbourIndicies = adjacency(secondMarbleIndex);
+        final int thirdMarbleIndex = secondMarbleNeighbourIndicies[dir];
+        // Die Richtung hat kein Feld, auf das die zweite Murmel gezogen werden kann:
+        if (thirdMarbleIndex == -1) return 0;
+        final long targetForSecondMarble = lookAtField(state, thirdMarbleIndex);
+        // Die dritte Murmel ist nicht vom selben Spieler
+        if (targetForSecondMarble != player) return 0;
+        
+        final int[] thirdMarbleNeighbourIndicies = adjacency(thirdMarbleIndex);
+        final int targetForThirdMarbleIndex = thirdMarbleNeighbourIndicies[dir];
+        // Die Richtung hat kein Feld, auf das die zweite Murmel gezogen werden kann:
+        if (targetForThirdMarbleIndex == -1) return 0;
+        final long targetForThirdMarble = lookAtField(state, targetForThirdMarbleIndex);
+        // Das Feld hat keinen Gegner
+        if (targetForThirdMarble != opponent) return 0;
+        
+        final int[] opponentNeighbourIndicies = adjacency(targetForThirdMarbleIndex);
+        final int emptyPlaceForOpponentMarbleIndex = opponentNeighbourIndicies[dir];
+        // Die Richtung hat kein Feld, auf das der Gegner gezogen werden kann:
+        if (emptyPlaceForOpponentMarbleIndex == -1) return 0;
+        final long emptyPlaceForOpponentMarble = lookAtField(state, emptyPlaceForOpponentMarbleIndex);
+        // Das Feld ist nicht leer
+        if (emptyPlaceForOpponentMarble != 0) return 0;
+        
+        state = move(state, opponent, targetForThirdMarbleIndex, emptyPlaceForOpponentMarbleIndex);
+        state = move(state, player, thirdMarbleIndex, targetForThirdMarbleIndex);
+        state = move(state, player, secondMarbleIndex, thirdMarbleIndex);
+        state = move(state, player, from, secondMarbleIndex);
+        
+        tempResult[moveCount] = state;
+        
+        return 1;
+    }
+}
