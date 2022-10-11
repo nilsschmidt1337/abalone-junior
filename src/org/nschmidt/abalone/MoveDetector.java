22740320bb292d157492fa7c78ac66d9bc987f39
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2022-10-11 20:29:36 +0200

    Simplified the concatenation of all moves.

--- src/org/nschmidt/abalone/MoveDetector.java
+++ src/org/nschmidt/abalone/MoveDetector.java
@@ -3,6 +3,7 @@ package org.nschmidt.abalone;
 import static org.nschmidt.abalone.DoubleAttacker.performDoubleAttack;
 import static org.nschmidt.abalone.TripleAttacker.performTripleAttack;
 import static org.nschmidt.abalone.TripleToDoubleAttacker.performTripleToDoubleAttack;
+
 import static org.nschmidt.abalone.TripleMover.moveThreeMarbles;
 import static org.nschmidt.abalone.DoubleMover.moveTwoMarbles;
 import static org.nschmidt.abalone.SingleMover.moveSingleMarble;
@@ -11,32 +12,25 @@ public enum MoveDetector {
     INSTANCE;
     
     public static long[] allMoves(long state, long player) {
-        final long[] singleMoves = moveSingleMarble(state, player);
-        final long[] doubleMoves = moveTwoMarbles(state, player);
-        final long[] tripleMoves = moveThreeMarbles(state, player);
-        final long[] doubleAttacks = performDoubleAttack(state, player);
-        final long[] tripleAttacks = performTripleAttack(state, player);
-        final long[] tripleToDoubleAttacks = performTripleToDoubleAttack(state, player);
-        
-        final int singleMoveCount = singleMoves.length;
-        final int doubleMoveCount = doubleMoves.length;
-        final int tripleMoveCount = tripleMoves.length;
-        final int doubleAttackCount = doubleAttacks.length;
-        final int tripleAttackCount = tripleAttacks.length;
-        final int tripleToDoubleAttackCount = tripleToDoubleAttacks.length;
-        final int singleAndDoubleMoveCount = singleMoveCount + doubleMoveCount;
-        final int totalMoveCount = singleAndDoubleMoveCount + tripleMoveCount;
-        final int totalMoveAndDoubleAttackCount = totalMoveCount + doubleAttackCount;
-        final int totalMoveAndDoubleAndTripleAttackCount = totalMoveCount + doubleAttackCount + tripleAttackCount;
-        
-        final long[] result = new long[totalMoveAndDoubleAndTripleAttackCount + tripleToDoubleAttackCount];
-        System.arraycopy(singleMoves, 0, result, 0, singleMoveCount);
-        System.arraycopy(doubleMoves, 0, result, singleMoveCount, doubleMoveCount);
-        System.arraycopy(tripleMoves, 0, result, singleAndDoubleMoveCount, tripleMoveCount);
-        System.arraycopy(doubleAttacks, 0, result, totalMoveCount, doubleAttackCount);
-        System.arraycopy(tripleAttacks, 0, result, totalMoveAndDoubleAttackCount, tripleAttackCount);
-        System.arraycopy(tripleToDoubleAttacks, 0, result, totalMoveAndDoubleAndTripleAttackCount, tripleToDoubleAttackCount);
-        
+        return join(moveSingleMarble(state, player), moveTwoMarbles(state, player), 
+                    moveThreeMarbles(state, player), performDoubleAttack(state, player),
+                    performTripleAttack(state, player), performTripleToDoubleAttack(state, player));
+    }
+    
+    private static long[] join(long[]... arrays) {
+        int length = 0;
+        for (long[] array : arrays) {
+            length += array.length;
+        }
+
+        final long[] result = new long[length];
+
+        int offset = 0;
+        for (long[] array : arrays) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+
         return result;
     }
 }
