6ed270fa9cdc32e4229b1421186e572e9310db87
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2022-12-29 22:23:36 +0100

    Added opening variants.

--- src/main/java/org/nschmidt/abalone/ai/HardcodedOpenings.java
+++ src/main/java/org/nschmidt/abalone/ai/HardcodedOpenings.java
@@ -35,8 +35,12 @@ public enum HardcodedOpenings {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HardcodedOpenings.class);
     
+    private static final Random RND = new Random();
+    
     private static final Map<Field, Field> BLACK_OPENINGS = new HashMap<>();
     private static final Map<Field, Field> WHITE_OPENINGS = new HashMap<>();
+    private static final Map<Field, Field> BLACK_VARIANT_OPENINGS = new HashMap<>();
+    private static final Map<Field, Field> WHITE_VARIANT_OPENINGS = new HashMap<>();
 
     private static void init() {
         if (!BLACK_OPENINGS.isEmpty() && !WHITE_OPENINGS.isEmpty()) {
@@ -50,6 +54,8 @@ public enum HardcodedOpenings {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(HardcodedOpenings.class.getResourceAsStream(OPENINGS_TXT), StandardCharsets.UTF_8))) {
             loadOpenings(reader, WHITE_OPENINGS);
             loadOpenings(reader, BLACK_OPENINGS);
+            loadOpenings(reader, WHITE_VARIANT_OPENINGS);
+            loadOpenings(reader, BLACK_VARIANT_OPENINGS);
         } catch (IOException e) {
             LOGGER.error("Can't load opening table.", e);
         }
@@ -98,9 +104,17 @@ public enum HardcodedOpenings {
         final Field currentField = Field.of(currentPieces);
         final Field result;
         if (player == WHITE) {
-            result = WHITE_OPENINGS.get(currentField);
+            if (RND.nextBoolean() || !WHITE_VARIANT_OPENINGS.containsKey(currentField)) {
+                result = WHITE_OPENINGS.get(currentField);
+            } else {
+                result = WHITE_VARIANT_OPENINGS.get(currentField);
+            }
         } else if (player == BLACK) {
-            result = BLACK_OPENINGS.get(currentField);
+            if (RND.nextBoolean() || !BLACK_VARIANT_OPENINGS.containsKey(currentField)) {
+                result = BLACK_OPENINGS.get(currentField);
+            } else {
+                result = BLACK_VARIANT_OPENINGS.get(currentField);
+            }
         } else {
             return null;
         }
@@ -108,23 +122,31 @@ public enum HardcodedOpenings {
         return result;
     }
     
-    private static final Random RND = new Random();
-    
     public static void main(String[] args) {
         LOGGER.info("Path for openings {}", HardcodedOpenings.class.getResource(OPENINGS_TXT).getPath());
         
         // Read openings
         init();
         
+        for (int i = 0; i < 10; i++) {
+            LOGGER.info("Iteration {}:", i);
+            searchForOpenings();
+        }
+    }
+
+    static void searchForOpenings() {
         final Map<Field, Field> openings;
+        final Map<Field, Field> variants;
         final Player player;
         if (RND.nextBoolean()) {
             LOGGER.info("Will generate a black opening move");
             openings = BLACK_OPENINGS;
+            variants = BLACK_VARIANT_OPENINGS;
             player = Player.BLACK;
         } else {
             LOGGER.info("Will generate a white opening move");
             openings = WHITE_OPENINGS;
+            variants = WHITE_VARIANT_OPENINGS;
             player = Player.WHITE;
         }
         
@@ -147,11 +169,14 @@ public enum HardcodedOpenings {
         .filter(move -> !openings.containsKey(move))
         .map(move -> {
             if (!WinningChecker.gainPiece(move, player)) {
-                return new Field[] {move, new AlphaBetaAI(6, player).bestMove(move)};
+                Field[] answers = new AlphaBetaAI(6, player).bestVariantMoves(move, 2);
+                if (answers.length == 0) return new Field[] {move, null};
+                if (answers.length == 1) return new Field[] {move, answers[0], answers[0]};
+                return new Field[] {move, answers[0], answers[1]};
             } else {
                 Field aggressiveMove = new AggressiveAlphaBetaAI(6, player).bestMove(move);
                 if (aggressiveMove != null && Field.countPieces(aggressiveMove, opponent) < Field.countPieces(move, opponent)) {
-                    return new Field[] {move, aggressiveMove};
+                    return new Field[] {move, aggressiveMove, aggressiveMove};
                 }
                 
                 return new Field[] {move, null};
@@ -169,12 +194,15 @@ public enum HardcodedOpenings {
         
         for(Field[] pair : newOpenings) {
             openings.put(pair[0], pair[1]);
+            variants.put(pair[0], pair[2]);
         }
 
         // Write openings
         try (PrintWriter writer = new PrintWriter(new FileOutputStream(HardcodedOpenings.class.getResource(OPENINGS_TXT).getPath()), false, StandardCharsets.UTF_8)) {
             writeOpenings(writer, WHITE_OPENINGS, Player.WHITE);
             writeOpenings(writer, BLACK_OPENINGS, Player.BLACK);
+            writeOpenings(writer, WHITE_VARIANT_OPENINGS, Player.WHITE);
+            writeOpenings(writer, BLACK_VARIANT_OPENINGS, Player.BLACK);
             writer.flush();
         } catch (IOException e) {
             LOGGER.error("Can't save opening table.", e);
@@ -188,9 +216,15 @@ public enum HardcodedOpenings {
         Player currentPlayer = Player.WHITE;
         Map<Field, Field> openings = WHITE_OPENINGS;
         for (int depth = 0; depth < targetDepth; depth++) {
-            if (openings.containsKey(currentField)) {
+            if (currentPlayer != targetPlayer && RND.nextBoolean()) {
+                Field[] moves = allMoves(currentField, currentPlayer);
+                if (moves.length > 0) {
+                    currentField = moves[RND.nextInt(moves.length)];
+                }
+            } else if (openings.containsKey(currentField)) {
                 currentField = openings.get(currentField);
             } else if (currentPlayer == targetPlayer) {
+                LOGGER.info("Find {} answer for all enemy responses to {}", targetPlayer, currentField);
                 return Optional.of(currentField);
             } else {
                 return Optional.empty();
